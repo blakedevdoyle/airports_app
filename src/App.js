e88f44899127dd564c3f753386746b0b420a000b
Blake <blake.doyle@hotmail.com.au>
2019-11-04 19:58:48 +1100

    push commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import './App.css';
 import { HomePage } from './components/index';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <HomePage />
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,40 +1,62 @@
 import React, { useState, useEffect } from 'react';
+import ReactLoading from 'react-loading';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './HomePage.scss';
 
 export const HomePage = () => {
-  const [loading, setLoading] = useState(true);
   const [airportData, setAirportData] = useState();
   const [apiError, setApiError] = useState();
 
-  useEffect(() => {
-    const fetchData = async() => {      
-      await axios.get('https://api.qantas.com/flight/refData/airport')
-      .then(res => setAirportData(res.data))
-      .then(setLoading(false))
-      .catch(err => setApiError(err));
-    };
+  // Fetch from url and store results in state and in localStorage
+  const fetchData = async() => {      
+    await axios.get('https://api.qantas.com/flight/refData/airport')
+    .then(res =>  {
+      setAirportData(res.data.splice(0,99));
+      localStorage.setItem("airports", JSON.stringify(res.data.splice(0,99)));
+    })
+    .catch(err => setApiError(err));
+  };
 
-    if (loading) {
-      fetchData();
+  // Check if there is localStorage. If not, runs fetchData function to fetch from url
+  useEffect(() => {    
+    const data = localStorage.getItem("airports");
+    if (data) {
+      setAirportData(JSON.parse(data));
+    } else {
+      setTimeout(() => { fetchData(); }, 3000)
     }
-  }, [loading]);
+  }, []);
 
-  console.log(airportData);
-
+  // If an error returns from the get request, log that to the screen
   if (apiError) {
     return <div>{apiError}</div>
   }
+
+  // If there is no error, map the objects to the screen
   if (!apiError) {
     return (
-      <div>
+      <div className="page-container">
+        <h2 className="page-title">Airports</h2>
         {airportData ? (
           airportData.map(item => (
-            <div key={item.airportCode}>
-              <span>{item.airportName}</span>
-              <span>{item.country.countryName}</span>
+            <div key={item.airportCode} className="airport-item-container">
+              <div className="details-container">
+                <span className="airport-name">{item.airportName}</span>
+                <span className="country-name">{item.country.countryName}</span>
+              </div>
+                <div className="view-item-button">
+                  <Link to={{
+                    pathname: `/airport/${item.airportCode}`,
+                    query:{
+                      airport: item,
+                    }}}>
+                      >
+                    </Link>
+                </div>
             </div>
           ))
-        ) : <h1>Loading component</h1>}
+        ) : <ReactLoading type="spin" color="red" height={50} width={50} className="loader" />}
       </div>
     );
   };
